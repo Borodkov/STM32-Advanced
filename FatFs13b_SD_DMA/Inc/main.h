503612de54d86653a85bb7be7fee1a4fcd6cb22c
Pavel Borodkov <pavel.borodkov@gmail.com>
2018-04-20 15:26:30 +0300

    FatFs 13b examples

---
+++ FatFs13b_SD_DMA/Inc/main.h
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  * @file    FatFs/FatFs_uSD_RTOS/Inc/main.h 
+  * @author  MCD Application Team
+  * @brief   Header for main.c module
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
+
+/* EVAL includes component */
+#include "stm32746g_discovery.h"
+
+/* FatFs includes component */
+#include "ff_gen_drv.h"
+#include "sd_diskio_dma.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+
+#endif /* __MAIN_H */
---
+++ FreeRTOS_v10_FatFs13b_SD/Inc/stm32f7xx_it.h
@@ -0,0 +1,41 @@
+/**
+  ******************************************************************************
+  * @file    FatFs/FatFs_uSD_RTOS/Inc/stm32f7xx_it.h 
+  * @author  MCD Application Team
+  * @brief   This file contains the headers of the interrupt handlers.
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32F7xx_IT_H
+#define __STM32F7xx_IT_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void MemManage_Handler(void);
+void BusFault_Handler(void);
+void UsageFault_Handler(void);
+void SVC_Handler(void);
+void DebugMon_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
+void BSP_SDMMC_IRQHandler(void);
+void BSP_SDMMC_DMA_Tx_IRQHandler(void);
+void BSP_SDMMC_DMA_Rx_IRQHandler(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32F7xx_IT_H */
